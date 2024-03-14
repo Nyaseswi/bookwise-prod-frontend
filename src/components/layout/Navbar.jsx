1617db69db94a72aea0e02b8200541bcb1fa5f0d
yaseswi.n23 <yaseswi.n23@gmail.com>
2024-03-14 11:20:33 +0530

    updated port number details

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     setShow(!show);
   };
   
-  const isDashboard = useLocation("http://localhost:5173/dashboard");
+  const isDashboard = useLocation("/dashboard");
   
   const { mode, setMode, isAuthenticated, user, setIsAuthenticated } = useContext(Context);
   
