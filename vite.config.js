aab9ec5f72239d0b217d81cf9060bf3c072770ed
yaseswi.n23 <yaseswi.n23@gmail.com>
2024-03-14 11:28:59 +0530

    added host command

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: parseInt(3000)
+    port: 8000,
   },
   plugins: [react()],
 
